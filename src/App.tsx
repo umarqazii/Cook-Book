16104bbbb8105a4b748d78334c30d3a0a42182eb
Umar Qazi <umarqazii983@gmail.com>
2024-08-19 00:04:20 +0500

    background, navbar, icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/navbar';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div> 
+      <Navbar />
       
     </div>
   );
