7c13679e4bab44ac9fb3ffd1d3d0a0d3c1638d89
Umar Qazi <umarqazii983@gmail.com>
2024-08-22 00:04:17 +0500

    viewing random as well as search based recipes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,19 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import axios from "axios"
+import {
+  ingredientOptions,
+  cuisineOptions,
+  mealTypeOptions,
+  dishTypeOptions,
+} from "../data/recipePageData";
 
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const fetchRecipes = async (query: string, selectedCuisineType: string) => {
+export const fetchRecipes = async (query: string, selectedCuisineType: string, selectedMealType: string, selectedDishType: string) => {
   //const appId = "ab7aeda7";
   const appId = process.env.REACT_APP_APPLICATION_ID;
   //const appKey = "582c50836c0a17b6c3b525cff3c88f63";
@@ -19,9 +25,44 @@ export const fetchRecipes = async (query: string, selectedCuisineType: string) =
       url += `&cuisineType=${selectedCuisineType}`;
   }
 
+  
+  if (selectedDishType) {
+    url += `&dishType=${selectedDishType}`;
+  }
+  if (selectedMealType) {
+      url += `&mealType=${selectedMealType}`;
+  }
+  
   try {
       const response = await axios.get(url);
-      console.log(response.data); // Handle the API response data
+      console.log(response.data.hits); // Handle the API response data
+      return response.data.hits;
+  } catch (error) {
+      console.error("Failed to fetch recipes:", error);
+      return [];
+  }
+};
+
+// fetch random recipes
+export const fetchRandomRecipes = async () => {
+  //const appId = "ab7aeda7";
+  const appId = process.env.REACT_APP_APPLICATION_ID;
+  //const appKey = "582c50836c0a17b6c3b525cff3c88f63";
+  const appKey = process.env.REACT_APP_APPLICATION_KEY;
+
+  let query = getRandomIngredient();
+  let selectedCuisineType = getRandomCuisineType();
+
+  console.log(query, selectedCuisineType);
+  
+  let url = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}&cuisineType=${selectedCuisineType}`;
+
+
+  try {
+      console.log(url);
+      console.log("fetching random recipes");
+      const response = await axios.get(url);
+      console.log(response.data.hits); // Handle the API response data
       return response.data.hits;
   } catch (error) {
       console.error("Failed to fetch recipes:", error);
@@ -32,4 +73,28 @@ export const fetchRecipes = async (query: string, selectedCuisineType: string) =
 //return the last 39 characters of the url
 export const extractRecipeId = (url: string) => {
   return url.slice(-39);
+}
+
+// function that returns a random ingredient from the ingredientOptions array
+const getRandomIngredient = () => {
+  const randomIndex = Math.floor(Math.random() * ingredientOptions.length);
+  return ingredientOptions[randomIndex].value;
+}
+
+// function that returns a random cuisine type from the cuisineOptions array
+const getRandomCuisineType = () => {
+  const randomIndex = Math.floor(Math.random() * cuisineOptions.length);
+  return cuisineOptions[randomIndex].value;
+}
+
+// function that returns a random meal type from the mealTypeOptions array
+const getRandomMealType = () => {
+  const randomIndex = Math.floor(Math.random() * mealTypeOptions.length);
+  return mealTypeOptions[randomIndex].value;
+}
+
+// function that returns a random dish type from the dishTypeOptions array
+const getRandomDishType = () => {
+  const randomIndex = Math.floor(Math.random() * dishTypeOptions.length);
+  return dishTypeOptions[randomIndex].value;
 }
--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -1,3 +1,4 @@
+//////////////////////importing the required files and packages////////////////////////
 import React, { useEffect, useState } from "react";
 import Select, { MultiValue, SingleValue } from "react-select";
 import makeAnimated from "react-select/animated";
@@ -9,8 +10,10 @@ import {
   mealTypeOptions,
   dishTypeOptions,
 } from "../data/recipePageData";
-import { fetchRecipes } from "../lib/utils";
+import { fetchRecipes, fetchRandomRecipes } from "../lib/utils";
+///////////////////////////////////////////////////////////////////////////////////////
 
+///////////////////////// interface declarations  //////////////////////////
 interface Recipe {
   label: string;
   image: string;
@@ -20,8 +23,10 @@ interface Recipe {
 interface Hit {
   recipe: Recipe;
 }
+///////////////////////////////////////////////////////////////////////////
 
 const Recipes = () => {
+  /////////////////////////// state declarations  //////////////////////////
   const [isOpen, setIsOpen] = React.useState(false);
   const toggleDrawer = () => {
     setIsOpen((prevState) => !prevState);
@@ -36,25 +41,80 @@ const Recipes = () => {
     label: string;
   } | null>(null);
 
-  const [recipes, setRecipes] = useState<Hit[]>([]);
+  const [selectedMealType, setSelectedMealType] = React.useState<{
+    value: string;
+    label: string;
+  } | null>(null);
 
+  const [selectedDishType, setSelectedDishType] = React.useState<{
+    value: string;
+    label: string;
+  } | null>(null);
+
+  const [recipes, setRecipes] = useState<Hit[]>([]);
+  const [randomrecipes, setRandomRecipes] = useState<Hit[]>([]);
+  //////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////// operations to be performed on the state //////////////////////////
   useEffect(() => {
     console.log(selectedIngredients);
     console.log(selectedCuisine);
   }, [selectedIngredients, selectedCuisine]);
 
   useEffect(() => {
+    const fetchAndSetRandomRecipes = async () => {
+      const hits = await fetchRandomRecipes();
+      setRandomRecipes(hits); // Update the recipes state with the fetched hits
+    };
+
+    fetchAndSetRandomRecipes();
+  }, []);
+
+  useEffect(() => {
     const fetchAndSetRecipes = async () => {
       const hits = await fetchRecipes(
         selectedIngredients.map((ingredient) => ingredient.value).join(","),
-        selectedCuisine?.value || ""
+        selectedCuisine?.value || "",
+        selectedMealType?.value || "",
+        selectedDishType?.value || ""
       );
       setRecipes(hits); // Update the recipes state with the fetched hits
     };
 
     fetchAndSetRecipes();
-  }, [selectedIngredients, selectedCuisine]);
+  }, [selectedIngredients, selectedCuisine, selectedMealType, selectedDishType]);
 
+  // Function to handle the change in the dropdown menu
+  const handleCuisineChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = event.target.value;
+    const selectedOption =
+      cuisineOptions.find((option) => option.value === selectedValue) || null;
+    setSelectedCuisine(selectedOption);
+  };
+
+  // Function to handle the change in the dropdown menu
+  const handleMealTypeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const selectedValue = event.target.value;
+    const selectedOption =
+      mealTypeOptions.find((option) => option.value === selectedValue) || null;
+    setSelectedMealType(selectedOption);
+  };
+
+  // Function to handle the change in the dropdown menu
+  const handleDishTypeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const selectedValue = event.target.value;
+    const selectedOption =
+      dishTypeOptions.find((option) => option.value === selectedValue) || null;
+    setSelectedDishType(selectedOption);
+  };
+
+  //////////////////////////////////////////////////////////////////////////
+
+  /////////////////////////// return statement //////////////////////////
   return (
     <div className="bg-gray-700 min-h-screen">
       <div className="max-w-screen-xl mx-auto p-4 flex flex-col">
@@ -64,24 +124,29 @@ const Recipes = () => {
         >
           Recipes
         </h1>
-        <div className="flex justify-center"> {/* Centering the button */}
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-auto"
-        onClick={toggleDrawer}
-      >
-        Apply Filters
-      </button>
-    </div>
+        <div className="flex justify-center mb-3">
+          {" "}
+          {/* Centering the button */}
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-auto"
+            onClick={toggleDrawer}
+          >
+            Browse Recipes
+          </button>
+        </div>
         <Drawer
           open={isOpen}
           onClose={toggleDrawer}
           direction="bottom"
           className="border rounded-t-2xl"
+          style={{ height: "80vh" }}
         >
           <div className="flex flex-col ">
-          <div className="flex justify-center">
-            <div className="bg-slate-400 mt-2 mb-2 sm:w-1/3 w-2/3 h-1 border rounded-lg"></div>
-          </div>
+            <div className="flex justify-center flex-col items-center">
+              <div className="bg-slate-400 mt-2 mb-2 sm:w-1/3 w-2/3 h-1 border rounded-lg"></div>
+              <h2 className="text-xl">Apply Filters</h2>
+              <div className="bg-slate-400 mt-2 mb-2 w-full h-0.3 border rounded-lg"></div>
+            </div>
             {/* dropdown menu to select multiple ingredients */}
             <p className="text-black text-center mb-1 ">Select Ingredients</p>
             <div className="flex justify-center mb-5">
@@ -91,69 +156,151 @@ const Recipes = () => {
                 defaultValue={[ingredientOptions[29]]}
                 isMulti
                 options={ingredientOptions}
-                className="w-full sm:w-1/2"
+                className="w-11/12 sm:w-1/2"
                 onChange={(selectedOptions) =>
                   setSelectedIngredients(selectedOptions)
                 }
               />
             </div>
+            <div className="flex justify-center">
+              <div className="bg-slate-400 mt-2 mb-2 w-full sm:w-3/5 h-0.3 border rounded-lg"></div>
+            </div>
             {/* dropdown menu to select a single cuisine */}
             <p className="text-black text-center mb-1">Select Cuisine</p>
             <div className="flex justify-center mb-5">
-              <Select
-                components={animatedComponents}
-                options={cuisineOptions}
-                className="w-full sm:w-1/2"
-                onChange={(selectedCuisine) =>
-                  setSelectedCuisine(
-                    selectedCuisine as SingleValue<{
-                      value: string;
-                      label: string;
-                    }>
-                  )
-                }
-                isClearable
-              />
+              <select
+                className="w-3/4 sm:w-1/2 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={handleCuisineChange}
+              >
+                <option value="">Select a cuisine</option>
+                {cuisineOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
             </div>
-            <div className="flex justify-center"> {/* Centering the button */}
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/5"
-        onClick={toggleDrawer}
-      >
-        Done
-      </button>
-    </div>
+
+            {/* dropdown menu to select a single meal type */}
+            <div className="flex justify-center">
+              <div className="bg-slate-400 mt-2 mb-2 w-full sm:w-3/5 h-0.3 border rounded-lg"></div>
+            </div>
+            <p className="text-black text-center mb-1">Select Meal Type</p>
+            <div className="flex justify-center mb-5">
+              <select
+                className="w-3/4 sm:w-1/2 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={handleMealTypeChange}
+              >
+                <option value="" className="text-gray-300">
+                  Select a meal type
+                </option>
+                {mealTypeOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {/* dropdown menu to select a single dish type */}
+            <div className="flex justify-center">
+              <div className="bg-slate-400 mt-2 mb-2 w-full sm:w-3/5 h-0.3 border rounded-lg"></div>
+            </div>
+            <p className="text-black text-center mb-1">Select Dish Type</p>
+            <div className="flex justify-center mb-5">
+              <select
+                className="w-3/4 sm:w-1/2 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={handleDishTypeChange}
+              >
+                <option value="">Select a dish type</option>
+                {dishTypeOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="flex justify-center">
+              {" "}
+              {/* Centering the button */}
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/5"
+                onClick={toggleDrawer}
+              >
+                Done
+              </button>
+            </div>
           </div>
         </Drawer>
         <div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            {/* Dynamic grid items from API */}
-            {recipes.map((recipeData, index) => {
-              const { label, image, url } = recipeData.recipe;
+          {recipes.length === 0 ? (
+            <>
+            <div className="text-white text-center mb-3">Here are some Random Recipes for You</div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              {/* Dynamic grid items from API */}
+              {randomrecipes.map((recipeData, index) => {
+                const { label, image, url } = recipeData.recipe;
 
-              return (
-                <div
-                  key={index}
-                  className="bg-gray-800 rounded-lg p-4 text-center"
-                >
-                  <h2 className="text-xl text-white font-semibold">{label}</h2>
-                  <img
-                    src={image}
-                    alt={label}
-                    className="mt-2 rounded-lg w-full object-cover"
-                  />
-                  <a
-                    href={url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="block text-blue-400 mt-2 "
+                return (
+                  <div
+                    key={index}
+                    className="bg-gray-800 rounded-lg p-4 text-center"
                   >
-                    View Recipe
-                  </a>
-                </div>
-              );
-            })}
-          </div>
+                    <h2 className="text-xl text-white font-semibold">
+                      {label}
+                    </h2>
+                    <img
+                      src={image}
+                      alt={label}
+                      className="mt-2 rounded-lg w-full h-4/5 object-cover"
+                    />
+                    <a
+                      href={url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="block text-blue-400 mt-2 "
+                    >
+                      View Recipe
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
+            </>
+            
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              {/* Dynamic grid items from API */}
+              {recipes.map((recipeData, index) => {
+                const { label, image, url } = recipeData.recipe;
+
+                return (
+                  <div
+                    key={index}
+                    className="bg-gray-800 rounded-lg p-4 text-center"
+                  >
+                    <h2 className="text-xl text-white font-semibold">
+                      {label}
+                    </h2>
+                    <img
+                      src={image}
+                      alt={label}
+                      className="mt-2 rounded-lg w-full h-4/5 object-cover"
+                    />
+                    <a
+                      href={url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="block text-blue-400 mt-2 "
+                    >
+                      View Recipe
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </div>
