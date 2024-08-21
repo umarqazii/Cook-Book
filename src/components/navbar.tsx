68246ebe3925d9920c5e7529e095898f72154ee4
Umar Qazi <umarqazii983@gmail.com>
2024-08-21 06:57:35 +0500

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -23,12 +23,12 @@ const Navbar = () => {
             </span>
           </Link>
           <div className="flex md:order-2 space-x-3 rtl:space-x-reverse">
-            <button
+            {/* <button
               type="button"
               className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center"
             >
               Get started
-            </button>
+            </button> */}
             <button
               onClick={toggleMenu}
               type="button"
