15a8727bca56e45f11c349da8517453e8e8a4b25
Umar Qazi <umarqazii983@gmail.com>
2024-08-28 10:33:13 +0500

    created a custom hook for resonponsiveness among different size screens

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,26 @@
 import React, { useEffect } from "react";
 import Navbar from "../components/navbar";
 import Typed from "typed.js";
-import img from "../assets/foodplate.png";
+import { useResponsive } from "./useResponsive";
+import img1 from "../assets/foodplate.png";
+import img2 from "../assets/handholdingplate2.png";
 
 const Home = () => {
-  useEffect(() => {
+  const breakpoints = [480, 768, 1600]; // Example breakpoints: small, medium, large screens
+    const breakpointIndex = useResponsive(breakpoints);
+  
+  //   useEffect(() => {
+  //   var typedSkill = new Typed(".typedSkill", {
+  //     strings: ["Millions of Recipes on your Fingertips"],
+  //     typeSpeed: 70,
+  //   });
 
+  //   return () => {
+  //     typedSkill.destroy();
+  //   };
+  // }, []);
+
+  useEffect(() => {
     var typedSkill = new Typed(".typedSkill", {
       strings: ["Millions of Recipes on your Fingertips"],
       typeSpeed: 70,
@@ -16,23 +31,92 @@ const Home = () => {
     };
   }, []);
 
+
   return (
-<div className="flex flex-wrap min-h-screen">
+    <>
+    {breakpointIndex === 0 && 
+      <div className="flex flex-wrap min-h-screen">
+        {/* Image Section */}
+        <div className="w-full bg-gray-700 flex flex-col items-center">
+          <img
+            src={img2}
+            alt="hospital"
+            className="w-full"
+          />
+          <h1
+            className="text-5xl text-white mb-3 text-center"
+            style={{ fontFamily: '"Matemasie", cursive' }}
+          >
+            Welcome to your Cook Book!
+          </h1>
+          <div className="text-2xl text-white text-center">
+            <span
+              className="typedSkill text-xl"
+              style={{ fontFamily: '"Cedarville Cursive", cursive' }}
+            ></span>
+            </div>
+        </div>
 
-{/* Image Section */}
-<div className="w-full sm:w-1/2 bg-gray-700 flex items-center justify-center">
-  <img src={img} alt="hospital" className="ml-3 w-1/1 sm: object-cover" />
-</div>
+      </div>
+    }
+    {breakpointIndex === 1 && 
+      <div className="flex flex-wrap min-h-screen">
+      {/* Image Section */}
+      <div className="w-full sm:w-1/2 bg-gray-700 flex items-center justify-center">
+        <img
+          src={img1}
+          alt="hospital"
+          className="ml-3 w-1/1 sm: object-cover"
+        />
+      </div>
 
-{/* Text Section */}
-<div className="w-full sm:w-1/2 flex flex-col justify-center items-center bg-gray-700">
-  <h1 className="text-5xl text-white mb-3 text-center" style={{ fontFamily: '"Matemasie", cursive' }}>Welcome to your Cook Book!</h1>
-  <div className="text-2xl text-white text-center">
-    <span className="typedSkill text-xl" style={{ fontFamily: '"Cedarville Cursive", cursive' }}></span>
-  </div>
-</div>
+      {/* Text Section */}
+      <div className="w-full sm:w-1/2 flex flex-col justify-center items-center bg-gray-700">
+        <h1
+          className="text-5xl text-white mb-3 text-center"
+          style={{ fontFamily: '"Matemasie", cursive' }}
+        >
+          Welcome to your Cook Book!
+        </h1>
+        <div className="text-2xl text-white text-center">
+          <span
+            className="typedSkill text-xl"
+            style={{ fontFamily: '"Cedarville Cursive", cursive' }}
+          ></span>
+        </div>
+      </div>
+    </div>
+    }
+    {breakpointIndex === 2 && 
+      <div className="flex flex-wrap min-h-screen">
+      {/* Image Section */}
+      <div className="w-full sm:w-1/2 bg-gray-700 flex items-center justify-center">
+        <img
+          src={img1}
+          alt="hospital"
+          className="ml-3 w-1/1 sm: object-cover"
+        />
+      </div>
 
-</div>
+      {/* Text Section */}
+      <div className="w-full sm:w-1/2 flex flex-col justify-center items-center bg-gray-700">
+        <h1
+          className="text-5xl text-white mb-3 text-center"
+          style={{ fontFamily: '"Matemasie", cursive' }}
+        >
+          Welcome to your Cook Book!
+        </h1>
+        <div className="text-2xl text-white text-center">
+            <span
+              className="typedSkill text-xl"
+              style={{ fontFamily: '"Cedarville Cursive", cursive' }}
+            ></span>
+            </div>
+      </div>
+    </div>
+    }
+
+    </>
   );
 };
 
