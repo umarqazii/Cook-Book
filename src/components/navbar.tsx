78ed0d8cdb2ebfff532b15403850a62783a31bf1
Umar Qazi <umarqazii983@gmail.com>
2024-08-28 14:55:29 +0500

    change in file structure

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import "./navbar.css";
+import "../styling/navbar.css";
 
 const Navbar = () => {
   let logo = "/logo192.png";
